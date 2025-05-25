482fe8d11144be4c0475e14cb2ddbe3f061a0a0a
espectrx <gustavo.pfontana@gmail.com>
2025-05-25 17:45:16 -0300

    Update testeProcessamento.py

--- trabalho/modulo/testeProcessamento.py
+++ trabalho/modulo/testeProcessamento.py
@@ -3,7 +3,7 @@ from tkinter import filedialog
 from tkinter import Tk
 import cv2
 from processamento import extrair_dados_da_imagem, visualizar_resultados
-
+from recomendacao import recomendar_roupas
 
 if __name__ == "__main__":
     # Cria a janela para escolher o arquivo
@@ -33,4 +33,4 @@ if __name__ == "__main__":
                                       medidas['pouco_cabelo'],
                                       tom_de_cabelo,
                                       medidas['tom_de_olho'])
-
+            recomendar_roupas(medidas)
